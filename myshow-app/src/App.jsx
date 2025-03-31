c7a8bd55550cdbb501fdd6eee556c2a0205a9379
Das10177 <sdas92211@gmail.com>
2025-03-31 19:02:07 +0530

    After File now start of navbar

--- myshow-app/src/App.jsx
+++ myshow-app/src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React from 'react'
+import Navbar from './Routes/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
 }
+
+export default App
